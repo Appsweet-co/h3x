3d3cd542ca6251359134f09e48e2d1f03b2fc105
Derek Peruo <derek@appsweet.co>
2023-09-26 15:15:13 +0000

    feat: (index.js) Add implies()

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ const { SIX_HUNDRED_SQ_MILES } = H3_RESOLUTIONS;
 
 const props = yargs()
   .options({
-    lat: { alias: 'y', description: 'Latitude', type: 'number', nargs: 1, default: 0 },
-    lng: { alias: 'x', description: 'Longitude', type: 'number', nargs: 1, default: 0 },
-    res: { alias: 'r', description: 'H3 Resolution', type: 'number', nargs: 1, default: SIX_HUNDRED_SQ_MILES },
+    lat: { alias: 'y', description: 'Latitude', type: 'number', nargs: 1 },
+    lng: { alias: 'x', description: 'Longitude', type: 'number', nargs: 1 },
+    res: { alias: 'r', description: 'H3 Resolution', type: 'number', nargs: 1 },
     cell: { alias: 'c', description: 'H3 Cell', type: 'string', nargs: 1 },
+  })
+  .implies({
+    lat: ['lng'],
+    lng: ['lat'],
+    res: ['lat', 'lng'],
   })
   .epilog('See https://h3geo.org/docs for more info')
   .parse(hideBin(Bun.argv));
