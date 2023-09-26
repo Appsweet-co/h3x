7fac224445b86099b372c5693389f4f88fe4ef19
Derek Peruo <derek@appsweet.co>
2023-09-26 09:26:14 -0500

    refactor: (*) Format files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # h3x
+
 A command line tool for https://h3geo.org/
--- src/const.js
+++ src/const.js
@@ -1,6 +1,5 @@
 /** https://h3geo.org/docs/core-library/restable/#average-area-in-km2 */
 export const H3_RESOLUTIONS = Object.freeze({
-    SIX_HUNDRED_SQ_MILES: 4,
-    ONE_HUNDRED_SQ_MILES: 5,
-  });
-  
+  SIX_HUNDRED_SQ_MILES: 4,
+  ONE_HUNDRED_SQ_MILES: 5,
+});
